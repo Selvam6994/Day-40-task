1308939167bcffde76633c31db51ac835c97ae1d
Selvam6994 <selvamyoor@gmail.com>
2023-02-26 23:31:53 +0530

    Connected with atlas

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // const express = require("express"); // "type": "commonjs"
 import express from "express"; // "type": "module"
 import { MongoClient } from "mongodb";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 // import { room_details } from "./room_data.js";
 const app = express();
 
-const PORT = 4000;
-const MONGO_URL = "mongodb://127.0.0.1";
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
 const client = new MongoClient(MONGO_URL);
 await client.connect();
 console.log("Mongo is connected !!!  ");
